76f6a9825facb3fbd892eecb4e1b9e9c10a52bd9
Tam Le <lttam.vn@gmail.com>
2025-11-20 14:21:48 +0900

    tt
    tt

--- compile2.sh
+++ compile2.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #after install jemdoc_mathjax
+
 jemdoc code.jemdoc
 jemdoc index.jemdoc
 jemdoc professional.jemdoc
 jemdoc publication.jemdoc
-jemdoc JSPS_Funds.jemdoc
+
+#jemdoc JSPS_Funds.jemdoc
